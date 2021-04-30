ba12c4fbf1767af8291169faf3c564d61069698e
Ali Kabalan <ali.kabalan.1994@gmail.com>
2021-04-30 21:18:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,3 +12,28 @@ function wrapAdjective(flair="*") {
     return `You are ${flair }${adj}${flair}!`;
   }
 }
+
+
+const Calculator = {
+  add: function(a, b) {
+    return a+b;
+  },
+  subtract: function(a, b) {
+    return a-b;
+  },
+  multiply: function(a, b) {
+    return a*b;
+  },
+  divide: function(a, b) {
+    return a/b;
+  }
+}
+
+function actionApplyer(start, fns) {
+  for (let i = 0; i < fns.length; i++) {
+    start = fns[i](start);
+  }
+  return start;
+}
+
+
